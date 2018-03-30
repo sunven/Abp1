cc55b5cc1cb76e97c6fd440cba786bedeb48d3a4
孙文权 <604164227@qq.com>
2018-03-30 14:15:46 +0800

    add FastDFSDemo

---
+++ FastDFSDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using FastDFS.Client;
+
+namespace FastDFSDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stream = new FileStream(@"D:\1.jpg", FileMode.Open);
+
+            ConnectionManager.Initialize(new List<IPEndPoint>{new IPEndPoint(IPAddress.Parse("172.16.2.154"), 22122) });
+            var g = FastDFSClient.GetStorageNode("group2");
+            var bytes = new byte[stream.Length];
+            stream.Read(bytes, 0, bytes.Length);
+            //FastDFSClient.UploadFile(g, b, "jpg");
+            Console.ReadKey();
+        }
+    }
+}
