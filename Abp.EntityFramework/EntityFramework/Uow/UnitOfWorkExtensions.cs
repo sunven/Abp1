d6ee3823b9efa57e7c33453139ec54d68904d3af
孙文权 <604164227@qq.com>
2018-04-11 18:26:44 +0800

    add UnitOfWorkExtensions

---
+++ Abp.EntityFramework/EntityFramework/Uow/UnitOfWorkExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.Entity;
+using Abp.Domain.Uow;
+using Abp.MultiTenancy;
+
+namespace Abp.EntityFramework.EntityFramework.Uow
+{
+    /// <summary>
+    /// UnitOfWork扩展
+    /// </summary>
+    public static class UnitOfWorkExtensions
+    {
+        /// <summary>
+        /// 获取DbContext作为活动工作单元的一部分。
+        /// 当前工作单元是<see cref ="EfUnitOfWork"/>时，可以调用此方法。
+        /// </summary>
+        /// <typeparam name="TDbContext">Type of the DbContext</typeparam>
+        /// <param name="unitOfWork">Current (active) unit of work</param>
+        public static TDbContext GetDbContext<TDbContext>(this IActiveUnitOfWork unitOfWork)
+            where TDbContext : DbContext
+        {
+            return GetDbContext<TDbContext>(unitOfWork, null);
+        }
+
+        public static TDbContext GetDbContext<TDbContext>(this IActiveUnitOfWork unitOfWork, MultiTenancySides? multiTenancySide)
+            where TDbContext : DbContext
+        {
+            if (unitOfWork == null)
+            {
+                throw new ArgumentNullException(nameof(unitOfWork));
+            }
+
+            if (!(unitOfWork is EfUnitOfWork))
+            {
+                throw new ArgumentException("unitOfWork is not type of " + typeof(EfUnitOfWork).FullName, nameof(unitOfWork));
+            }
+
+            return (unitOfWork as EfUnitOfWork).GetOrCreateDbContext<TDbContext>(multiTenancySide);
+        }
+    }
+}
